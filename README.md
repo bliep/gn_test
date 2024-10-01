944732d71419d1f68ee99472c0eed750a7c9ce87
Anuj Dev <anujdev@gmail.com>
2024-10-01 14:51:41 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# TESTing 123
+
+The service listens to port 15001 by default and handles GET and POST requests as follows:
+
+## GET
+```
+curl localhost:15001
+```
+will return a list of found Bluetooth devices as e.g.:
+```
+(base) adev@mac ~ % curl localhost:15001
+addr=70:BF:92:D0:DC:63, connected=False, Jabra Elite Active 75t
+addr=70:F2:7A:EE:E6:63, connected=False, 
+addr=48:E1:5C:64:BC:8F, connected=False, 
+addr=37:F2:D2:9E:8D:95, connected=False, 
+addr=65:C9:73:F0:38:78, connected=False, 
+addr=64:A2:62:F9:BE:15, connected=False, 
+addr=4D:98:F9:F0:D2:A0, connected=False, 
+addr=57:62:50:FD:41:4C, connected=False, 
+addr=65:74:F4:01:3C:08, connected=False, 
+addr=6C:64:C4:55:52:A2, connected=False, 
+addr=5C:05:03:87:A3:82, connected=False, 
+addr=6C:70:CB:8F:2D:89, connected=False, 65" QLED
+addr=E2:BB:9E:8E:55:4D, connected=False, ET-2820 Series
+
+```
+Note that not every device has a name embedded in their advertisement packet, so the name not always there.
+
+## POST
+```
+curl -v -X POST -H 'Content-Type: application/json' -d '{"addr": "70:BF:92:D0:DC:63", "connected": true}' localhost:15001
+```
+will try to set the connected state of the device mentioned by addr to `true` (and similarly to `false` below).
+```
+curl -v -X POST -H 'Content-Type: application/json' -d '{"addr": "70:BF:92:D0:DC:63", "connected": false}' localhost:15001
+```
+The HTTP response gives the result of the connection or disconnection operation and is one of 
+ - `HttpStatusCode.NotFound` if the addr is not found in the bluetooth scan table
+ - `HttpStatusCode.Unauthorized` if the connection or disconnection is unsucessful.
+ - `HttpStatusCode.OK` if the operation succeeded.
+ - `HttpStatusCode.ServiceUnavailable` if the server failed bad.
+
+
+Enjoy ;)
---
+++ service.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+using System.Linq;
+using Linux.Bluetooth;
+using Linux.Bluetooth.Extensions;
+
+using System.Text.Json;
+public class DeviceState {
+    public String? addr  { get; set; }
+    public bool? connected { get; set; }
+}
+
+
+class Program
+{
+    private static readonly int port = 15001;
+    private static Adapter? adapter = null;
+    
+    static async Task Main(string[] args)
+    {
+        Task backgroundTask = Task.Run(() => BackgroundWorker());
+
+        HttpListener listener = new();
+        listener.Prefixes.Add($"http://localhost:{port}/");
+        listener.Start();
+        Console.WriteLine($"Listening for requests on http://localhost:{port}/");
+
+        while (true) {
+            HttpListenerContext context = await listener.GetContextAsync();
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            if (request.HttpMethod == "GET") {
+                await HandleGET(response);
+            } else if (request.HttpMethod == "POST") {
+                await HandlePOST(request, response);
+            } else {
+                response.StatusCode = (int)HttpStatusCode.MethodNotAllowed;
+            }
+            response.Close();
+        }
+    }
+
+    // curl -v localhost:15001
+    private static async Task HandleGET(HttpListenerResponse response)
+    {
+        if (adapter == null) {
+            response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+            return ;
+        }
+
+        string responseString = "";
+
+        var devices = await adapter.GetDevicesAsync();
+        foreach (var device in devices) {
+            var deviceProperties = await device.GetAllAsync();
+            responseString += $"addr={deviceProperties.Address}, connected={deviceProperties.Connected}, {deviceProperties.Name}\n";
+        }
+
+        byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+        response.ContentLength64 = buffer.Length;
+        response.StatusCode = (int)HttpStatusCode.OK;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+    }
+
+    private static async Task HandlePOST(HttpListenerRequest request, HttpListenerResponse response)
+    {
+        if (adapter == null) {
+            response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+            return ;
+        }
+
+        using var reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding);
+        string requestBody = await reader.ReadToEndAsync();
+        DeviceState? deviceState = JsonSerializer.Deserialize<DeviceState>(requestBody);
+        if (deviceState == null || deviceState.addr == null || deviceState.connected == null) {
+            response.StatusCode = (int)HttpStatusCode.ServiceUnavailable;
+            return ;
+        }
+
+        Console.WriteLine($"JSON parsed {deviceState.addr} {deviceState.connected}");
+
+        Device? device = await adapter.GetDeviceAsync(deviceState.addr);
+        if (device == null) {
+            response.StatusCode = (int)HttpStatusCode.NotFound;
+            return;
+        }
+        var deviceProperties = await device.GetAllAsync();
+        if (deviceProperties.Connected != deviceState.connected) {
+            if (deviceState.connected == true) {
+                await device.ConnectAsync();
+            } else {
+                await device.DisconnectAsync();
+            }
+        }
+        deviceProperties = await device.GetAllAsync();
+        if (deviceProperties.Connected == deviceState.connected) {
+            response.StatusCode = (int)HttpStatusCode.OK;
+        } else {
+            response.StatusCode = (int)HttpStatusCode.Unauthorized;
+        }
+    }
+
+    private static async Task BackgroundWorker()
+    {
+        var adapters = await BlueZManager.GetAdaptersAsync();
+        if (adapters.Count == 0) {
+            Console.WriteLine("No Bluetooth adapters found.");
+            return;
+        }
+        adapter = adapters[0];
+
+        while (true) {
+            await adapter.StartDiscoveryAsync();
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            await adapter.StopDiscoveryAsync();
+        }
+    }
+}
